1d60416ecb11c0c6babfe47990349c9819f69f21
helene6789 <lenehe099@gmail.com>
2025-04-11 10:55:06 +0200

    user : config et entité sauf relation

---
+++ ProjetYoga.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetYoga.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string Salt { get; set; } = string.Empty;
+
+        // public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+    }
+}
---
+++ ProjetYoga.Infrastructure/Configs/UserConfig.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjetYoga.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetYoga.Infrastructure.Configs
+{
+    internal class UserConfig : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.Id)
+                   .IsRequired()
+                   .ValueGeneratedOnAdd();
+
+            builder.Property(u => u.Email)
+                   .IsRequired()
+                   .HasMaxLength(255);
+
+            builder.HasIndex(u => u.Email)
+                   .IsUnique();
+
+            builder.Property(u => u.PasswordHash)
+                   .IsRequired();
+
+            builder.Property(u => u.Salt)
+                   .IsRequired();
+
+       //     builder.HasMany(u => u.Reservations)
+       //.WithOne(r => r.User)
+       //.HasForeignKey(r => r.UserId)
+       //.OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
