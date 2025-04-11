4a61701c1b8cede24e6395865080a057e183d8ac
helene6789 <lenehe099@gmail.com>
2025-04-11 11:56:16 +0200

    Instructor : entité et config

---
+++ ProjetYoga.Domain/Entities/Instructor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetYoga.Domain.Entities
+{
+    public class Instructor
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Nickname { get; set; } = "Instructor";  // Valeur par défaut "Instructor"
+    }
+}
---
+++ ProjetYoga.Infrastructure/Configs/InstructorConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjetYoga.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetYoga.Infrastructure.Configs
+{
+    internal class InstructorConfig : IEntityTypeConfiguration<Instructor>
+    {
+        public void Configure(EntityTypeBuilder<Instructor> builder)
+        {
+            // Configuration de la table Instructor
+            builder.ToTable("Instructor"); 
+
+            // La clé primaire est héritée de la classe User, donc pas besoin de la redéfinir ici.
+
+            // Configuration de la propriété Nickname
+            builder.Property(i => i.Nickname)
+                   .IsRequired()                // Non nullable
+                   .HasMaxLength(50)            // Taille maximale
+                   .HasDefaultValue("Instructor");  // Valeur par défaut "Instructor"
+        }
+    }
+}
