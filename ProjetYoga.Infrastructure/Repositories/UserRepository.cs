eb5778a993cc81de4bd70ffe73603128bb68db8a
helene6789 <lenehe099@gmail.com>
2025-04-15 14:30:48 +0200

    UserRepository  héritant de RepositoryBase pf User

---
+++ ProjetYoga.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Be.Khunly.EFRepository;
+using ProjetYoga.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetYoga.Infrastructure.Repositories
+{
+    public class UserRepository(ProjetYogaContext context)
+        : RepositoryBase<User>(context)
+    {
+    }
+}
