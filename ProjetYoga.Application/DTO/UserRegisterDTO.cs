0123e49413bbddb23d35ff3777d771b4905a55eb
helene6789 <lenehe099@gmail.com>
2025-04-15 15:21:02 +0200

    UserService début - RegisterUserDTO

---
+++ ProjetYoga.Application/DTO/UserRegisterDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetYoga.Application.DTO
+{
+    public class UserRegisterDTO
+    {
+        [MaxLength(400)]
+        [EmailAddress]
+        public string email { get; set; } = null!;
+        public string password { get; set; } = null!;
+    }
+}
