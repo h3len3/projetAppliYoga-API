e97c8d942afa7f8ed6e5fcfada253f0f5df31967
helene6789 <lenehe099@gmail.com>
2025-04-15 17:15:45 +0200

    ajouter UserController et début méthode post

---
+++ ProjetYoga.API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjetYoga.Application.DTO;
+using ProjetYoga.Application.Interfaces.Services;
+
+namespace ProjetYoga.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(IUserService userService) : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] UserRegisterDTO dto)
+        {
+            try
+            {
+
+            }
+            catch (Exception) 
+            {
+                throw;
+            }
+
+        }
+    }
+}
