a32438e9d0d523c6a77f902e225cb5304f385132
helene6789 <lenehe099@gmail.com>
2025-04-15 15:36:36 +0200

    DuplicationPropertyException

---
+++ ProjetYoga.Application/Exceptions/DuplicatePropertyException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetYoga.Application.Exceptions
+{
+    public class DuplicatePropertyException(string propertyName): Exception($"La propriété {propertyName} doit être unique")
+    {
+    }
+}
