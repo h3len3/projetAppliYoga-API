1b4aaac3c6236b5a6e376970e682b38a5fcc1d08
helene6789 <lenehe099@gmail.com>
2025-04-15 16:39:00 +0200

    correction ds UserService ds insérer membre

--- ProjetYoga.Application/Services/UserService.cs
+++ ProjetYoga.Application/Services/UserService.cs
@@ -2,6 +2,7 @@
 using ProjetYoga.Application.Exceptions;
 using ProjetYoga.Application.Interfaces.Repositories;
 using ProjetYoga.Application.Utils;
+using ProjetYoga.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,17 +21,21 @@ namespace ProjetYoga.Application.Services
                 throw new DuplicatePropertyException(nameof(dto.email));
             }
             // vérifier règles mot de passe
+
             // Traitement mdp : 
             Guid salt = Guid.NewGuid();
             string hashedPassword = PasswordUtils.HashPassword(dto.password, salt);
+            
             //insérer ce user
-            userRepository.Add(new Domain.Entities.User
+            User u = userRepository.Add(new Domain.Entities.User
             {
                 Email = dto.email,
-                Password = dto.password,
+                Password = hashedPassword,
                 Salt = salt
             });
             // envoyer un mail à ce user
+
+            return u ;
         }
     }
 }
