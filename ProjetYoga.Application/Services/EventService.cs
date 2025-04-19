7915f26e033b6349dd4ae247a1eddaafb592ece6
helene6789 <lenehe099@gmail.com>
2025-04-19 11:25:57 +0200

    EventService : début - CreateEventDTO : début

---
+++ ProjetYoga.Application/Services/EventService.cs
@@ -0,0 +1,21 @@
+﻿using ProjetYoga.Application.DTO;
+using ProjetYoga.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetYoga.Application.Services
+{
+    public class EventService(IEventRepository eventRepository)
+    {
+        // créer un Event <-> règles métier
+        public void CreateEvent(CreateEventDTO dto)
+        {
+            // vérifier toutes les règles de création : pas vraiment dans ce cas
+            //enregistrer, dans la DB
+            
+        }
+    }
+}
