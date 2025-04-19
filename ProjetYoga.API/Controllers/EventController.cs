8df0d3cdd996f40ebe4cd1a33c7c6c9fbb03ff28
helene6789 <lenehe099@gmail.com>
2025-04-19 11:30:25 +0200

    EventController : début méthode Post(CreatEventDTO dto)

--- ProjetYoga.API/Controllers/EventController.cs
+++ ProjetYoga.API/Controllers/EventController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProjetYoga.Application.DTO;
+using ProjetYoga.Application.Services;
 
 namespace ProjetYoga.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class EventController : ControllerBase
+    public class EventController(EventService eventService) : ControllerBase
     {
+        [HttpPost]
+        public IActionResult Post(CreateEventDTO dto)
+        {
+            try
+            {
+                eventService.CreateEvent(dto);
+                return Created();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
